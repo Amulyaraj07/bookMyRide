a0aea10f7d8f4b8fcae80e5047240301232a0651
Amulyaraj07 <amulyaraj1234@gmail.com>
2025-07-29 15:39:48 +0530

    added UserContext

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,8 @@ import UserSignup from './pages/UserSignup'
 import UserLogin from './pages/UserLogin'
 import CaptainSignup from './pages/CaptainSignup'
 import CaptainLogin from './pages/CaptainLogin'
+import Start from './pages/Start'
+import UserProtectWrapper from './pages/UserProtectWrapper'
 
 
 
@@ -12,11 +14,18 @@ const App = () => {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Home/>} />
+        <Route path="/" element={<Start/>} />
         <Route path="/signup" element={<UserSignup/>} />
         <Route path="/login" element={<UserLogin/>} />
         <Route path="/captain-signup" element={<CaptainSignup/>} />
         <Route path="/captain-login" element={<CaptainLogin/>} />
+        <Route path="/home" element={<UserProtectWrapper>
+          <Home/>
+        </UserProtectWrapper>} />
+
+        <Route path='/user/logout' element={<UserProtectWrapper>
+          
+        </UserProtectWrapper>}/>
       </Routes>
     </div>
   )
