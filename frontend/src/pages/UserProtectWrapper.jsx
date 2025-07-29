3cf1a631428af32d045815185410fb494ed5c6b7
Amulyaraj07 <amulyaraj1234@gmail.com>
2025-07-29 20:52:08 +0530

    made some changes for better security

--- frontend/src/pages/UserProtectWrapper.jsx
+++ frontend/src/pages/UserProtectWrapper.jsx
@@ -2,19 +2,42 @@ import React, { useEffect } from 'react'
 import { useContext } from 'react'
 import { UserDataContext } from '../context/UserContext.jsx'
 import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import { useState } from 'react'
 
 
 const UserProtectWrapper = ({children}) => {
     const token = localStorage.getItem('token')
 
     const navigate = useNavigate()
+    const {user,setUser} = useContext(UserDataContext)
+    const [isLoading, setIsLoading] = useState(true)
 
     useEffect(() => {
         if (!token) {
             navigate('/login')
         }
+
+        axios.get(`${import.meta.env.VITE_API_URL}/users/profile`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }).then((response) => {
+            if (response.status === 200) {
+                setUser(response.data.user)
+                setIsLoading(false)
+            }
+        }).catch((error) => {
+            console.error("Error fetching user profile:", error)
+            localStorage.removeItem('token')
+            navigate('/login')
+        })
     }
     , [token])
+
+    if (isLoading) {
+        return <div>Loading...</div>
+    }
 
   return (
     <div>
